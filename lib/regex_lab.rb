efd61acf1bfa359c31ae7727282be0302f084509
Josh Burd <Jburdt@gmail.com>
2022-11-13 18:48:15 -0500

    commit tree 2a34d0faee7a916731c642ebdec396daba10813a

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,12 @@
-def starts_with_a_vowel?(word)
+require "pry"
 
+def starts_with_a_vowel?(word)
+word.match(/^[aeiouAEIOU]/)
+binding.pry
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+^
 end
 
 def words_five_letters_long(text)
